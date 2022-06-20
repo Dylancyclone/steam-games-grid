c92071cbc9ba1dd9b9044a79641e6da72ee37f29
Dylan Lathrum <dylanlathrum@gmail.com>
2022-06-19 18:47:38 -0700

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+q-page.flex.flex-center
+	q-header.row.justify-around.q-pa-md(elevated)
+		q-input(dark dense standout v-model='search')
+			template(#append)
+				q-icon(name="search")
+</template>
+
+<script>
+import { defineComponent } from "vue"
+
+export default defineComponent({
+	name: "PageIndex",
+
+	data() {
+		return {
+			search: "",
+		}
+	},
+})
+</script>
